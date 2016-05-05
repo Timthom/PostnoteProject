31e5b895b9adf9263c4144a950d683fe9561e043
Cragnarsson <iths@Cissis-Apples-MacBook-Pro.local>
2016-05-05 11:44:28 +0200

    creatorComponent

--- src/client/app/creator.component.ts
+++ src/client/app/creator.component.ts
@@ -16,6 +16,23 @@ import {FirebaseListObservable} from 'angularfire2';
 ])
 
 export class CreatorComponent {
+    title: string = ""; 
+    text: string = ""; 
+    //notes: Note[] = []; 
     
+    notes: String[] = []; 
+    
+    save() {
+        if(this.title !== ""){
+            console.log(this.title);
+            console.log(this.text);
+            this.notes.splice(0,0, this.title);
+            
+            
+            console.log(this.notes);
+            this.title = ""; 
+            this.text = "";
+        }
+    }
     
 }
