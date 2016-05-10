dd1580b67df3f5d82a0f309da036ba45fa99fae0
Ermin Mahmutovic <iths@Apples-MacBook-Pro-139.local>
2016-05-10 15:30:41 +0200

    adding onclick scroll

--- src/client/app/menu.component.ts
+++ src/client/app/menu.component.ts
@@ -19,13 +19,18 @@ import {Note}from './note';
 
 export class MenuComponent {
     
-    titles :FirebaseListObservable<Note[]>;
+    titles :FirebaseListObservable<any[]>;
 
     constructor(private _af:AngularFire){
       this.titles = this._af.list('/notes');
 
     }
-    jumpToNote(note:Note){
-      console.log("Loggar ut note ",note);
+    jumpToNote(note:string){
+      
+      var element = document.getElementById(note);
+      
+      element.scrollIntoView(true);
+      
+      console.log("Loggar ut note ", element);
     }
 }
