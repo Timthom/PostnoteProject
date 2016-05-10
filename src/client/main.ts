7d5e0aab26aca9bb262501a61734a1b2d827ded3
Benjamin Holmlid <benhol84@gmail.com>
2016-05-10 20:42:54 +0200

    Added routing_providers in the main.ts (necassary for the routing between components to work)

--- src/client/main.ts
+++ src/client/main.ts
@@ -4,12 +4,13 @@ import {environment} from './app/environment';
 import {Postnote2App} from './app/postnote2.component';
 import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire} from 'angularfire2/angularfire2';
 import 'rxjs/Rx';
+import {ROUTER_PROVIDERS} from "angular2/router";
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(Postnote2App,  [
- FIREBASE_PROVIDERS,
+bootstrap(Postnote2App, [ROUTER_PROVIDERS, FIREBASE_PROVIDERS,
  defaultFirebase('https://dazzling-fire-7472.firebaseio.com')
 ]);
+  
