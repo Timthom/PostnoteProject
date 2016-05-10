b1d454a59a2122b8cddeb0cce39ec24fcb6e00e0
Felix Olofsson <foson95@gmail.com>
2016-05-10 13:28:04 +0200

    added edit button functionality to note.component

--- src/client/app/note.component.ts
+++ src/client/app/note.component.ts
@@ -2,6 +2,7 @@ import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {AngularFire} from 'angularfire2';
 import {FirebaseListObservable} from 'angularfire2';
+import {DataService} from './data.service';
 //added
 import {Input} from 'angular2/core';
 import {Note} from './note';
@@ -19,7 +20,43 @@ import {Note} from './note';
 ])
 
 export class NoteComponent {
+  
+  constructor(private _ds: DataService) {
+    console.log('in constructor');  
+  }
+  
    @Input()
    noteInNote; 
+  
+  //title: string = this.noteInNote.title;
+  //text: string = this.noteInNote.text;
+  isEditable: boolean = false;
+    
+    editClick() {
+      console.log(this.isEditable);
+      var titleField = document.getElementById('title');
+      var textField = document.getElementById('text_field');
+      
+      if(this.isEditable) {
+        console.log('updating');
+        titleField.style.border = "";
+        textField.style.border = "";
+        
+        console.log('title text: '+ titleField.innerText);
+        console.log('text text: '+ textField.innerText);
+        //this._ds.updateNoteTitle(this.noteInNote.$key, titleField.innerHTML);
+        //this._ds.updateNoteText(this.noteInNote.$key, textField.innerHTML);
+      } else {
+        console.log('notdating');
+        titleField.style.border = "dashed 1px black";
+        textField.style.border = "dashed 1px black";
+        textField.focus();
+      }
+      this.isEditable = !this.isEditable;  
+      var x = document.activeElement.tagName;
+      console.log(x);
+      console.log('key: ' + this.noteInNote.$key);
+      console.log(this.noteInNote.title);
+    }
     
 }
