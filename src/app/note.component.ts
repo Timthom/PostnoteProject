7c30b51302fd6f5ec5ab42d43e1c50c5076cfe12
UffeO <uffe_93@outlook.com>
2016-05-11 15:37:29 +0200

    fixed issues

--- src/app/note.component.ts
+++ src/app/note.component.ts
@@ -30,40 +30,35 @@ export class NoteComponent{
   text;
   
   constructor(private _ds: DataService) {
-<<<<<<< HEAD:src/app/note.component.ts
     console.log('in constructor'); 
     //varför undefined här?...
     console.log(this.noteInNote);
-=======
-    console.log('in constructor');
->>>>>>> Uffe:src/client/app/note.component.ts
   }
 
   isEditable: boolean = false;
     
     editClick() {
       
+      /*
       var titleField = document.getElementsByClassName[0]('title');
-      var textField = document.getElementsByClassName[0]('text_field');
-      
-      console.log(titleField);
-      console.log(textField);
+      var textField = document.getElementsByClassName[0]('text_field');*/
       
       if(this.isEditable) {
-        console.log('updating');
+        console.log('updating');/*
         titleField.style.border = "";
         textField.style.border = "";
         titleField.style.pointerEvents = "none";
-        textField.style.pointerEvents = "none";
+        textField.style.pointerEvents = "none";*/
         this._ds.updateNoteTitle(this.noteInNote.$key, this.title);
         this._ds.updateNoteText(this.noteInNote.$key, this.text);
       } else {
         console.log('notdating');
+        /*
         titleField.style.border = "dashed 1px black";
         textField.style.border = "dashed 1px black";
         textField.style.width = "278px";
         titleField.style.pointerEvents = "auto";
-        textField.style.pointerEvents = "auto";
+        textField.style.pointerEvents = "auto";*/
       }
       this.isEditable = !this.isEditable;  
       
