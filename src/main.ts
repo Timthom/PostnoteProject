acc9ff2208825b4dbb6652e76e0a2b33571d795a
Benjamin Holmlid <benhol84@gmail.com>
2016-05-15 23:59:08 +0200

    Prepared main.ts for AuthorizationService

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './app/';
 import {AppComponent} from './app/app/';
 import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire} from 'angularfire2/angularfire2';
+// import {AuthorizationService} from "./app/authorization.service";
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrap(AppComponent, [
- FIREBASE_PROVIDERS,
+ FIREBASE_PROVIDERS, /* AuthorizationService */, 
  defaultFirebase('https://dazzling-fire-7472.firebaseio.com')
 ]);
+
