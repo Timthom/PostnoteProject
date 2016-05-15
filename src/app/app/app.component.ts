52e0bd6918df694a3d04aaad52d89a9f089b0e01
Benjamin Holmlid <benhol84@gmail.com>
2016-05-16 00:28:40 +0200

    Added html routes in app.component.html

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -17,16 +17,17 @@ import {CreateUserAccountComponent} from '../createuser.components/create-user-a
   directives: [ROUTER_DIRECTIVES, MenuComponent],
   pipes: []
 })
+
 @RouteConfig([
    {path: '/route1', name:'Login', component: LoginComponent, useAsDefault : true},
    {path: '/route2', name:'AllNotes', component: Postnote2App},
    {path: '/route3', name:'CreateUserAccount', component: CreateUserAccountComponent}
 ])
+
 export class AppComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
+  
 }
