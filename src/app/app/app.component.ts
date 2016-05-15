6b2cb0a28d1a90150755f8265b850a71fdce38b6
Benjamin Holmlid <benhol84@gmail.com>
2016-05-16 00:12:03 +0200

    Added routing to app.component.ts

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -3,7 +3,10 @@ import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router-
 import {AngularFire} from 'angularfire2';
 import {FirebaseListObservable} from 'angularfire2';
 import {Postnote2App} from '../postnote2.component';
+import {MenuComponent} from '../menu.component';
 import {DataService} from '../data.service';
+import {LoginComponent} from '../login.components/login.component';
+import {CreateUserAccountComponent} from '../createuser.components/create-user-account.component';
 
 @Component({
   moduleId: module.id,
@@ -11,10 +14,13 @@ import {DataService} from '../data.service';
   providers: [ROUTER_PROVIDERS, DataService, AngularFire],
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ROUTER_DIRECTIVES, Postnote2App],
+  directives: [ROUTER_DIRECTIVES, MenuComponent],
   pipes: []
 })
 @RouteConfig([
+   {path: '/route1', name:'Login', component: LoginComponent, useAsDefault : true},
+   {path: '/route2', name:'AllNotes', component: Postnote2App},
+   {path: '/route3', name:'CreateUserAccount', component: CreateUserAccountComponent}
 ])
 export class AppComponent implements OnInit {
 
