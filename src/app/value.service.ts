943982d6f3d422de3c2cd7662bb73142e65ce71b
Ermin Mahmutovic <iths@Apples-MacBook-Pro-139.local>
2016-05-26 14:20:32 +0200

    adding code

--- src/app/value.service.ts
+++ src/app/value.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class ValueService {
 
   _showSideBar:boolean = true;
-  _toggleExpand:boolean = true;
+  _toggleExpand:boolean = false;
 
   constructor() {}
   
