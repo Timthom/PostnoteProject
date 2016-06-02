7bed061ddb78368a2a1ed0e70f3d6df110a207d9
Marcus <iths@Apples-MacBook-Pro-7.local>
2016-06-02 13:24:53 +0200

    now the database syncs when dragin to another group...

--- src/app/dragula-helper.service.ts
+++ src/app/dragula-helper.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { DragulaService } from 'ng2-dragula/ng2-dragula';
+import {DataService} from './data.service';
 
 @Injectable()
 export class DragulaHelperService {
 
-  constructor() {}
+  constructor(private _dataservice: DataService) {}
 
   /***********************************************************
   -------------------Dragula stuff below----------------------
@@ -13,23 +14,57 @@ export class DragulaHelperService {
   _configureDragula(dragulaService: DragulaService) {
     dragulaService.setOptions('drag-bag', {
       isContainer: function (el) {
-        // console.log('isContainer el: '+el);
+        /* Denna skriver ut själva notedivobjectet, här kommer man åt alla element inom notes... */
+        // console.log('isContainer el: ');
+        // console.log(el);
+        
         return false; // only elements in drake.containers will be taken into account
       },
       moves: function (el, source, handle, sibling) {
-        // console.log(`moves, el: ${el} source: ${source} handle: ${handle} sibling: ${sibling}`);
+       /* 
+       el är samma som ovan, den note man håller och alla dess element 
+       source kan man se div.classname och alla els inom den diven, inte bara den man håller
+       handle är det element där man tog tag i noten. man kommer åt allt inom också...
+       sibling är den som ligger till höger om noten... om längst till höger så är sibling null...
+       */ 
+      //  console.log(`moves, el: `);
+      //  console.log(el);
+      //  console.log(`moves, source: `);
+      //  console.log(source);
+      //  console.log(`moves, handle: `);
+      //  console.log(handle);
+      //  console.log(`moves, sibling: `);
+      //  console.log(sibling);
+       
         return true; // elements are always draggable by default
       },
       accepts: function (el, target, source, sibling) {
-        // console.log(`accepts, el: ${el} target: ${target}  source: ${source} sibling: ${sibling}`);
+       /* 
+       enda nya är target som är det elementet där den släpps...
+       */ 
+      //  console.log(`accepts, el: `);
+      //  console.log(el);
+      //  console.log(`accepts, target: `);
+      //  console.log(target);
+      //  console.log(`accepts, source: `);
+      //  console.log(source);
+      //  console.log(`accepts, sibling: `);
+      //  console.log(sibling); 
+        
         return true; // elements can be dropped in any of the `containers` by default
       },
       invalid: function (el, handle) {
-        // console.log(`invalid, el: ${el} handle: ${handle}`);
-        // console.log(`el.tagname: ${el.tagName}`);
+      /* 
+      inget nytt...
+      */ 
+      //  console.log(`invalid, el: `);
+      //  console.log(el);
+      //  console.log(`invalid, handle: `);
+      //  console.log(handle);
+       
         return false; // don't prevent any drags from initiating by default
       },
-      direction: 'horizontal',             // Y axis is considered when determining where an element would be dropped
+      direction: 'horizontal',           // Y axis is considered when determining where an element would be dropped
       copy: false,                       // elements are moved by default, not copied
       copySortSource: false,             // elements in copy-source containers can be reordered
       revertOnSpill: false,              // spilling will put the element back where it was dragged from, if this is true
@@ -39,48 +74,85 @@ export class DragulaHelperService {
     });
     
     dragulaService.drag.subscribe((value) => {
-      // console.log(`drag: ${value[0]}`);
-      this._onDrag(value.slice(1));
+      /* 
+      man får ut en array med tre index...
+      [0]: namnet på containern...
+      [1]: note.elementet som drogs...
+      [2]: diven som den dras ifrån...
+      */ 
+      //  console.log(`drag, value: `);
+      //  console.log(value);
+      
+      
+      
+
+
     });
     dragulaService.drop.subscribe((value) => {
-      // console.log(`drop: ${value[0]}`);
-      this._onDrop(value.slice(1));
+      /* 
+      man får ut en array med fem index...
+      [0]: namnet på containern...
+      [1]: note.elementet som drogs...
+      [2]: diven som den släpps i...
+      [3]: diven som den dras ifrån...
+      [4]: Sibling, den till höger om där den släpptes... om sist null...
+      */ 
+      //  console.log(`drop, value: `);
+      //  console.log(value);
+       
+       //Detta ger mig en sträng med id:t...
+       //console.log(value[1].attributes[3].nodeValue);
+       
+       //Detta ger mig namnet på gruppen, varsam på att om man lägger i creator så är den tom...
+       //  console.log(value[2].parentElement.firstElementChild.id);
+       //  console.log(value[2].parentElement.firstElementChild.id === "");
+       
+      let id: string = value[1].attributes[3].nodeValue;
+      let group: string;
+      if (value[2].parentElement.firstElementChild.id === '') {
+        group = 'noGroup'
+      } else {
+        group = value[2].parentElement.firstElementChild.id;
+      }
+      
+      this._dataservice.changeNoteGroup(id, group)
+      
     });
     dragulaService.over.subscribe((value) => {
-      // console.log(`over: ${value[0]}`);
-      this._onOver(value.slice(1));
+      /* 
+      man får ut en array med fyra index...
+      [0]: namnet på containern...
+      [1]: note.elementet som drogs...
+      [2]: containern. som den är över ifrån...
+      [3]: containers som den drogs ifrån...
+      */ 
+      //  console.log(`over, value: `);
+      //  console.log(value);
+      
+      
+      
+      
+      
+
+
     });
     dragulaService.out.subscribe((value) => {
-      // console.log(`out: ${value[0]}`);
-      this._onOut(value.slice(1));
+      /* 
+      man får ut en array med fyra index...
+      [0]: namnet på containern...
+      [1]: note.elementet som drogs...
+      [2]: containern. som den är över ifrån...
+      [3]: containers som den drogs ifrån...
+      */ 
+      //  console.log(`out, value: `);
+      //  console.log(value);
+
+      
+      
+      
+
     });
     
   }
   
-  private _onDrag(args) {
-    let [e, el] = args;
-    // do something
-  //  console.log(`Inne i onDrag e: ${e}, el: ${el}`);
-  }
-
-  private _onDrop(args) {
-    let [e, el] = args;
-    // do something
-    // console.log(`Inne i onDrop e: ${e}, el: ${el}`);
-  }
-
-  private _onOver(args) {
-    let [e, el, container] = args;
-    // do something
-    // console.log(`Inne i onOver e: ${e}, el: ${el}, container: ${container}`);
-  }
-
-  private _onOut(args) {
-    let [e, el, container] = args;
-    // do something
-    // console.log(`Inne i onOut e: ${e}, el: ${el}, container: ${container}`);
-  }
-   
-  
-
 }
