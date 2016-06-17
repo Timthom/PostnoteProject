4587604e240b1e2f56fee4bd0c1992c9068106ca
Cragnarsson <iths@Cissis-Apples-MacBook-Pro.local>
2016-06-17 17:02:24 +0200

    deleted more console.logs

--- src/app/note.ts
+++ src/app/note.ts
@@ -1,6 +1,5 @@
 export class Note {
   
   constructor(private title: string, private text: string, private group: string, private $key: string, private color: string){
-      console.log("inne i note.ts konstruktor");
   }
 }
